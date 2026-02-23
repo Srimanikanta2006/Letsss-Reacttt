b670f4a3b989860ba75636f4a2b5ef9d53668274
psmk <srimanikantapothu@gmail.com>
2026-02-23 19:59:33 +0530

    Added Local Storage Functionality, End.

--- 10-TodoContextWithLocal/src/App.jsx
+++ 10-TodoContextWithLocal/src/App.jsx
@@ -1,19 +1,17 @@
 import "./App.css";
+import { TodoForm } from "./Components";
 import { TodoProvider } from "./Contexts/TodoContext";
-import { useState } from "react";
-
+import { use, useEffect, useState } from "react";
+import TodoItem from "./Components/TodoItem";
 function App() {
-  const { todos, setTodos } = useState([]);
+  const [todos, setTodos] = useState([]);
   const addTodo = (todo) => {
     setTodos((prev) => [{ id: Date.now(), ...todo }, ...prev]);
     //here we are adding the new todo to the previous list of todos, and we are also giving it a unique id using Date.now() function.
   };
   const updateTodo = (id, todo) => {
     setTodos(
-      (prev) =>
-        prev.map((prevTodo) => {
-          prevTodo.id === id ? todo : prevTodo;
-        }),
+      (prev) => prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo)),
       //here we are updating the todo with the given id, and we are returning the updated todo if the id matches, otherwise we are returning the previous todo.
     );
   };
@@ -32,15 +30,37 @@ function App() {
     //here we are toggling the completed property of the todo with the given id, and we are returning the updated todo if the id matches, otherwise we are returning the previous todo.
   };
   //here the name should be same as the one in the context provider, then the functionality works.
+
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem("todos"));
+    if (todos && todos.length > 0) {
+      setTodos(todos);
+    }
+  }, []);
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+  //we can use many useEffects.
   return (
     <TodoProvider
       value={{ todos, addTodo, deleteTodo, updateTodo, toggleCompleted }}
     >
-      <div className="flex flex-wrap min-h-screen items-center">
-        <div className="w-full">
-          <div className="w-full max-w-sm mx-auto flex justify-end mb-4"></div>
-
-          <div className="w-full max-w-sm mx-auto"></div>
+      <div className="bg-[#172842] min-h-screen py-8">
+        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+          <h1 className="text-2xl font-bold text-center mb-8 mt-2">
+            Manage Your Todos
+          </h1>
+          <div className="mb-4">
+            {/* Todo form goes here */}
+            <TodoForm />
+          </div>
+          <div className="flex flex-wrap gap-y-3">
+            {todos.map((todo) => (
+              <div className="w-full" key={todo.id}>
+                <TodoItem todo={todo} />
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </TodoProvider>
---
+++ 10-TodoContextWithLocal/src/Components/index.js
@@ -0,0 +1,4 @@
+import TodoForm from "./TodoForm";
+import TodoItem from "./TodoItem";
+export { TodoForm, TodoItem };
+//normally index js files are used to export all the components from a folder, so that we can import them easily in other files. For example, instead of importing TodoForm and TodoItem separately in App.jsx, we can import them together from the Components folder.
