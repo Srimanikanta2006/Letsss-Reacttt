8758a779b01d61f1013e1eea8ed3434f631ad20f
Srimanikanta2006 <192361116+Srimanikanta2006@users.noreply.github.com>
2026-05-03 00:15:02 +0530

    Add Pages

--- 12-MegaBlog/src/components/index.js
+++ 12-MegaBlog/src/components/index.js
@@ -5,4 +5,23 @@ import Container from "./container/container";
 import Logo from "./logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import RTE from "./RTE";
-export { Header, Footer, Container, Logo, LogoutBtn, RTE };
+import Signup from "./Signup";
+import Login from "./Login";
+import Post from "./Post";
+import PostForm from "./post-form/PostForm";
+import PostCard from "./PostCard";
+import AuthLayout from "./AuthLayout";
+export {
+  Header,
+  Footer,
+  Container,
+  Logo,
+  LogoutBtn,
+  RTE,
+  Signup,
+  Login,
+  Post,
+  PostForm,
+  PostCard,
+  AuthLayout,
+};
